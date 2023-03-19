adebbec4f799b0383d31e08185f2f24f5f184144
Felpower <felbauer.patrick@gmail.com>
2023-03-19 22:10:40 +0100

    removed square app

--- lib/app_bar_widgets/remove_adds_button.dart
+++ lib/app_bar_widgets/remove_adds_button.dart
@@ -2,7 +2,6 @@ import 'package:bachelor_flutter_crush/bloc/user_state_bloc/dark_patterns_bloc/d
 import 'package:bachelor_flutter_crush/bloc/user_state_bloc/dark_patterns_bloc/dark_patterns_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../bloc/reporting_bloc/reporting_bloc.dart';
--- lib/controllers/payment_controller.dart
+++ lib/controllers/payment_controller.dart
@@ -7,6 +7,7 @@ import 'package:bachelor_flutter_crush/bloc/reporting_bloc/reporting_event.dart'
 
 import '../bloc/reporting_bloc/reporting_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' as flutter_bloc;
+
 class PaymentController extends StatefulWidget {
   const PaymentController({Key? key}) : super(key: key);
 
@@ -75,23 +76,24 @@ class PaymentState extends State<PaymentController> {
                 ),
                 CreditCardWidget(
                   glassmorphismConfig:
-                  useGlassMorphism ? Glassmorphism.defaultConfig() : null,
+                      useGlassMorphism ? Glassmorphism.defaultConfig() : null,
                   cardNumber: cardNumber,
                   expiryDate: expiryDate,
                   cardHolderName: cardHolderName,
                   cvvCode: cvvCode,
                   bankName: 'Axis Bank',
                   frontCardBorder:
-                  !useGlassMorphism ? Border.all(color: Colors.grey) : null,
+                      !useGlassMorphism ? Border.all(color: Colors.grey) : null,
                   backCardBorder:
-                  !useGlassMorphism ? Border.all(color: Colors.grey) : null,
+                      !useGlassMorphism ? Border.all(color: Colors.grey) : null,
                   showBackView: isCvvFocused,
                   obscureCardNumber: true,
                   obscureCardCvv: true,
                   isHolderNameVisible: true,
                   cardBgColor: AppColors.cardBgColor,
-                  backgroundImage:
-                  useBackgroundImage ? 'assets/images/credit/card_bg.png' : null,
+                  backgroundImage: useBackgroundImage
+                      ? 'assets/images/credit/card_bg.png'
+                      : null,
                   isSwipeGestureEnabled: true,
                   onCreditCardWidgetChange:
                       (CreditCardBrand creditCardBrand) {},
@@ -208,23 +210,24 @@ class PaymentState extends State<PaymentController> {
 
   void _onValidate() {
     if (formKey.currentState!.validate()) {
-          Navigator.pop(context, 'Ok');
-    print("Payment Complete");
-    showDialog(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-              title: const Text("Successfully Removed Adds"),
-              content: const Text(
-                  "Thank you for purchasing the Remove Adds Function, you are getting charged for 2,99€ per month from now on!"),
-              actions: <Widget>[
-                TextButton(
-                    onPressed: () => {Navigator.pop(context, 'Ok')},
-                    child: const Text('Ok')),
-              ],
-            ));
-    final reportingBloc = flutter_bloc.BlocProvider.of<ReportingBloc>(context);
-    reportingBloc.add(ReportPaidForRemovingAddsEvent(true));
-    updateSharedPreferences();
+      Navigator.pop(context, 'Ok');
+      print("Payment Complete");
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+                title: const Text("Successfully Removed Adds"),
+                content: const Text(
+                    "Thank you for purchasing the Remove Adds Function, you are getting charged for 2,99€ per month from now on!"),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () => {Navigator.pop(context, 'Ok')},
+                      child: const Text('Ok')),
+                ],
+              ));
+      final reportingBloc =
+          flutter_bloc.BlocProvider.of<ReportingBloc>(context);
+      reportingBloc.add(ReportPaidForRemovingAddsEvent(true));
+      updateSharedPreferences();
     } else {
       print('invalid!');
     }
@@ -250,79 +253,3 @@ class PaymentState extends State<PaymentController> {
     }
   }
 }
-
-// import 'package:bachelor_flutter_crush/bloc/reporting_bloc/reporting_event.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:square_in_app_payments/in_app_payments.dart';
-// import 'package:square_in_app_payments/models.dart';
-//
-// import '../bloc/reporting_bloc/reporting_bloc.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart' as flutter_bloc;
-//
-// class PaymentController extends GetxController {
-//   Map<String, dynamic>? paymentIntentData;
-//   late BuildContext context;
-//
-//   void makePayment(BuildContext context) {
-//     this.context = context;
-//
-//     InAppPayments.setSquareApplicationId(
-//         'sandbox-sq0idb-tGCIx1hiRafOVCkzw5V6XA');
-//     InAppPayments.startCardEntryFlow(
-//         onCardNonceRequestSuccess: _cardNonceRequestSuccess,
-//         onCardEntryCancel: _cardEntryCancel);
-//   }
-//
-//   void _cardEntryCancel() {
-//     Navigator.pop(context, 'Ok');
-//     showDialog(
-//         context: context,
-//         builder: (BuildContext context) => AlertDialog(
-//               title: const Text("Payment Failed"),
-//               content: const Text("The payment action was cancelled"),
-//               actions: <Widget>[
-//                 TextButton(
-//                     onPressed: () => {Navigator.pop(context, 'Ok')},
-//                     child: const Text('Ok'))
-//               ],
-//             ));
-//   }
-//
-//   void _cardNonceRequestSuccess(CardDetails result) {
-//     print("nonce: " + result.nonce);
-//
-//     InAppPayments.completeCardEntry(onCardEntryComplete: _cardEntryComplete);
-//   }
-//
-//   void _cardEntryComplete() {
-//     Navigator.pop(context, 'Ok');
-//     print("Payment Complete");
-//     showDialog(
-//         context: context,
-//         builder: (BuildContext context) => AlertDialog(
-//               title: const Text("Successfully Removed Adds"),
-//               content: const Text(
-//                   "Thank you for purchasing the Remove Adds Function, you are getting charged for 2,99€ per month from now on!"),
-//               actions: <Widget>[
-//                 TextButton(
-//                     onPressed: () => {Navigator.pop(context, 'Ok')},
-//                     child: const Text('Ok')),
-//               ],
-//             ));
-//     final reportingBloc = flutter_bloc.BlocProvider.of<ReportingBloc>(context);
-//     reportingBloc.add(ReportPaidForRemovingAddsEvent(true));
-//     updateSharedPreferences();
-//   }
-//
-//   void updateSharedPreferences() async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     bool? addActive = prefs.getBool("addsActive");
-//     if (addActive == true) {
-//       prefs.setBool('addsActive', false);
-//     } else {
-//       prefs.setBool('addsActive', true);
-//     }
-//   }
-// }
