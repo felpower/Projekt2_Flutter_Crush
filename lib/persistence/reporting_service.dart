e4bf6ae127e0ca8eac3a40c2ae5bd17974c13e87
Felpower <felbauer.patrick@gmail.com>
2023-03-19 15:31:54 +0100

    update appwrite

--- lib/persistence/reporting_service.dart
+++ lib/persistence/reporting_service.dart
@@ -2,14 +2,15 @@ import 'dart:developer' as dev;
 import 'dart:math';
 
 import 'package:appwrite/appwrite.dart';
-import 'package:appwrite/models.dart';
+import 'package:appwrite/models.dart' as models;
 import 'package:bachelor_flutter_crush/persistence/dark_patterns_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ReportingService {
   static const String collectionId = '6416edda75723d0674ab';
-  static const String endpointUrl = 'http://localhost/v1';
+  static const String endpointUrl = 'http://192.168.0.80/v1';
   static const String projectId = '6416ebbc5895408f82e8';
+  static const String databaseId = '6416ed0281be171b2ec1';
 
   static const String uuid = 'uuid';
   static const String darkPatterns = 'darkPatterns';
@@ -25,52 +26,65 @@ class ReportingService {
   static final Random _rnd = Random();
 
   static Client client = Client();
-  static Database database = Database(client);
+  static Databases database = Databases(client);
   static Account account = Account(client);
 
   static Future<void> addAdvertisementTap(double x, double y) async {
-    await _updateDocumentData(addScreenClick, 'x: ' + x.toStringAsFixed(2) + ', y:' + y.toStringAsFixed(2));
+    await _updateDocumentData(addScreenClick,
+        'x: ' + x.toStringAsFixed(2) + ', y:' + y.toStringAsFixed(2));
   }
 
   static Future<void> addStartOfLevel(int levelNumber) async {
-    await _updateDocumentData(startOfLevel,'Level: ' + levelNumber.toString() + ', time: ' + DateTime.now().toString());
+    await _updateDocumentData(
+        startOfLevel,
+        'Level: ' +
+            levelNumber.toString() +
+            ', time: ' +
+            DateTime.now().toString());
   }
 
   static Future<void> addStartApp(DateTime dateTime) async {
     await _updateDocumentData(bootAppStartTime, dateTime.toString());
   }
 
-  static Future<void> addCloseApp(DateTime dateTime) async{
+  static Future<void> addCloseApp(DateTime dateTime) async {
     await _updateDocumentData(closeAppTime, dateTime.toString());
   }
 
-  static Future<void> addRating(double rating) async{
+  static Future<void> addRating(double rating) async {
     await _updateDocumentData(ratingApp, rating.toString());
   }
 
-  static Future<void> addNotificationTap(DateTime dateTime, String? multiplier) async {
+  static Future<void> addNotificationTap(
+      DateTime dateTime, String? multiplier) async {
     multiplier ??= '';
-    await _updateDocumentData(notificationTap, 'Multiplier: ' + multiplier + ', Time: ' + dateTime.toString());
+    await _updateDocumentData(notificationTap,
+        'Multiplier: ' + multiplier + ', Time: ' + dateTime.toString());
   }
 
-  static Future<void> _updateDocumentData(String documentPropertyName, String data) async {
-    Document document = await database.getDocument(
-        collectionId: collectionId, documentId: await _getUuid());
+  static Future<void> _updateDocumentData(
+      String documentPropertyName, String data) async {
+    models.Document document = await database.getDocument(
+        collectionId: collectionId,
+        documentId: await _getUuid(),
+        databaseId: databaseId);
     List<dynamic> dataList = document.data[documentPropertyName];
     List<String> documentData = dataList.cast<String>();
     dataList.add(data);
 
-    database.updateDocument(collectionId: collectionId, documentId: await _getUuid(), data: {
-      documentPropertyName: documentData
-    });
+    database.updateDocument(
+        collectionId: collectionId,
+        documentId: await _getUuid(),
+        databaseId: databaseId,
+        data: {documentPropertyName: documentData});
   }
 
   static Future<void> init() async {
     _initClient();
 
-    SessionList? sessions;
+    models.SessionList? sessions;
     try {
-      sessions = await account.getSessions();
+      sessions = await account.listSessions();
     } catch (e) {
       dev.log(e.toString());
       dev.log('session not found');
@@ -82,19 +96,23 @@ class ReportingService {
   }
 
   static void _addDocumentIfItDoesNotExist() async {
-    Document? document;
+    models.Document? document;
     try {
       document = await database.getDocument(
-          collectionId: collectionId, documentId: await _getUuid());
+          collectionId: collectionId,
+          databaseId: databaseId,
+          documentId: await _getUuid());
     } catch (e) {
       dev.log(e.toString());
       dev.log("could not find file");
     }
 
+    print("Printed Document" + document.toString());
     if (document != null) {
       return;
     }
-    bool shouldDarkPatternsBeVisible = await DarkPatternsService.shouldDarkPatternsBeVisible();
+    bool shouldDarkPatternsBeVisible =
+        await DarkPatternsService.shouldDarkPatternsBeVisible();
     final planetsByDiameter = {
       uuid: await _getUuid(),
       darkPatterns: shouldDarkPatternsBeVisible,
@@ -103,27 +121,35 @@ class ReportingService {
       addScreenClick: [],
       startOfLevel: [],
       notificationTap: [],
-      ratingApp:[]
+      ratingApp: []
     };
 
-    database.createDocument(
+    var createDocument = database.createDocument(
         collectionId: collectionId,
         documentId: await _getUuid(),
+        databaseId: databaseId,
         data: planetsByDiameter);
+
+    print("Printed Document" + createDocument.toString() + "planetsByDiameter: " + planetsByDiameter.toString());
   }
 
   static void _initClient() {
-    client.setEndpoint(endpointUrl).setProject(projectId).setSelfSigned(status: true);
+    client
+        .setEndpoint(endpointUrl)
+        .setProject(projectId)
+        .setSelfSigned(status: true);
   }
 
   static Future<String> _getUuid() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     String? response = prefs.getString(uuid);
     if (response != null) {
+      print(response.toString());
       return response;
     }
-    final newUuid = _getRandomString(15);
+    final newUuid = ID.unique();
     prefs.setString(uuid, newUuid);
+    print(newUuid.toString());
     return newUuid;
   }
 
