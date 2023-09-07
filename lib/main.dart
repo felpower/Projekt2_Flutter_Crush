5d96ff43bf81e71340c21842c329c18779ba04f9
Felpower <felbauer.patrick@gmail.com>
2023-09-07 11:56:28 +0200

    removed # from url

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:bachelor_flutter_crush/application.dart';
 import 'package:bachelor_flutter_crush/persistence/reporting_service.dart';
 import 'package:bachelor_flutter_crush/services/local_notification_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
 import 'firebase_options.dart';
 import 'helpers/audio.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Audio.init();
+  usePathUrlStrategy();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   ReportingService.init();
   await LocalNotificationService().init();
