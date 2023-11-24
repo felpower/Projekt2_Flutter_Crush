0f54ee4909ce53d03bf9cbaaf40487f7b942ed32
Felpower <felbauer.patrick@gmail.com>
2023-11-24 13:50:28 +0100

    added InfoPage for game

--- lib/controllers/device_token/device_token.dart
+++ lib/controllers/device_token/device_token.dart
@@ -19,15 +19,9 @@ class _DeviceTokenState extends State<DeviceToken> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Center(child: Text('Device Token')),
-          leading: BackButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+          title: const Center(child: Text('Instruktionen zum Spiel')),
         ),
-        body: Center(
-            child: Column(children: [
+        body: ListView( padding: const EdgeInsets.all(20), children: [
           Visibility(
             visible: false,
             child: FutureBuilder<String>(
@@ -77,21 +71,44 @@ class _DeviceTokenState extends State<DeviceToken> {
             child: TextField(
                 controller: autohrizationStatus, textAlign: TextAlign.center, readOnly: true),
           ),
-          Container(
-              alignment: Alignment.center,
-              width: 300,
-              child: const Text("Wenn Sie über 18 Jahre alt sind und an der "
-                  "Studie "
-                  "teilnehmen möchten,\n "
-                  "klicken Sie bitte auf „Ich bin mit der Studienteilnahme einverstanden.“ und "
-                  "bestätigen so Ihr Einverständnis für die Studienteilnahme. ")),
+          const Text('''Spielanleitung:
+1.	Spielbrett und Jellies: Das Spielbrett ist ein Gitter mit verschiedenen farbigen Jellies. Jede Jelly hat eine einzigartige Farbe und Form. (siehe Bild)
+'''),
+          const Image(image: AssetImage('assets/instructions/ins_1.png'), fit: BoxFit.cover),
+          const Text(
+              '''2.	Spielzüge: Tausche zwei benachbarte Jellies, um eine Reihe oder Spalte von mindestens drei gleichfarbigen Jellies zu bilden.'''),
+          const Image(image: AssetImage('assets/instructions/ins_2.png'), fit: BoxFit.cover),
+          const Text('''3.	Kombinationen und Bonusse:
+  •	Kombiniere 4 Jellies, um ein spezielles Jelly zu erhalten, das eine ganze Reihe oder Spalte löschen kann. 
+  Bild Sonderjelly einfügen!
+  Kombiniere 5 Jellies in einem T- oder L-Form, um ein Regenbogen-Jelly zu bekommen, das alle Jellies einer bestimmten Farbe vom Brett entfernt.
+  Bild Sonderjelly einfügen!
+'''),
+          const Text(
+              '''4.	Levelziele: Jedes Level hat spezifische Ziele, wie das Erreichen einer bestimmten Punktzahl, das Sammeln einer Anzahl von bestimmten Jellies oder das Entfernen von Hindernissen. Das jeweilige Ziel wird in dem Kasten rechts oben auf dem Bildschirm angezeigt'''),
+          const Image(image: AssetImage('assets/instructions/ins_3.png'), fit: BoxFit.cover),
+          const Text('''5.	Bewegungsbegrenzung und Zeitlimit: Einige Level haben eine begrenzte 
+          Anzahl von Zügen oder ein Zeitlimit, um die Ziele zu erreichen. Dies wird in dem Kasten links oben auf dem Bildschirm angezeigt.'''),
+          const Image(image: AssetImage('assets/instructions/ins_4.png'), fit: BoxFit.cover),
+          const Text('''
+6.	Booster: Nutze Booster bzw. Sonderjellies, um schwierige Level zu meistern. Diese können durch Spielverlauf oder Käufe (direkt vor dem Levelstart) erworben werden.
+
+7.	Fortschritt und Herausforderungen: Schalte neue Level und Herausforderungen frei, indem du im Spiel fortschreitest.
+
+Tipps und Tricks:
+•	Plane deine Züge voraus, um die effektivsten Kombinationen zu erstellen.
+•	Nutze die speziellen Jellies strategisch, um schwierige Bereiche zu meistern.
+•	Halte Ausschau nach unerwarteten Kettenreaktionen.
+
+Viel Spaß beim Spielen!
+'''),
           ElevatedButton(
-              child: const Text('Ich bin mit der Studienteilnahme einverstanden', textAlign:
-              TextAlign.center),
+              child: const Text('Spiel jetzt starten',
+                  textAlign: TextAlign.center),
               onPressed: () {
                 Navigator.pop(context);
               }),
-        ])));
+        ]));
   }
 
   Future<void> getNotification() async {
