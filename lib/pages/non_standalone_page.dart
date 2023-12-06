9d7b08dc340d95ca5cd11ba7154e817f2c693b7a
Felpower <felbauer.patrick@gmail.com>
2023-12-06 11:16:20 +0100

    added different install options

--- lib/pages/non_standalone_page.dart
+++ lib/pages/non_standalone_page.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:universal_html/html.dart';
 
-import 'package:flutter/material.dart';
-import 'package:pwa_install/pwa_install.dart';
+import '../helpers/device_helper.dart';
 
 class NonStandalonePage extends StatefulWidget {
   const NonStandalonePage({Key? key}) : super(key: key);
@@ -12,6 +12,8 @@ class NonStandalonePage extends StatefulWidget {
 
 class _BeforeInstallPrompt extends State<NonStandalonePage> {
   BeforeInstallPromptEvent? deferredPrompt;
+
+  bool isIosDevice = DeviceHelper.isIOSDevice();
 
   @override
   void initState() {
@@ -27,53 +29,37 @@ class _BeforeInstallPrompt extends State<NonStandalonePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "JellyFun",
+        title: "Herzlich Willkommen zur Studie",
         debugShowCheckedModeBanner: false,
         home: Scaffold(
             body: Center(
                 child: ListView(padding: const EdgeInsets.all(20), children: [
-          const Text(
-            'Bitte fügen Sie diese Seite auf Ihren Homescreen/Startbildschirm hinzu, '
-            'danach können Sie das Spiel wie jede andere App handhaben. Die Installation '
-            'funktioniert wie folgt: \n'
-            'für Android (alle Handys außer iPhone): Drücken Sie '
-            'auf die drei kleinen Punkte rechts oben auf dem Bildschirm -> App installieren',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                backgroundColor: Colors.white,
-                decoration: TextDecoration.none),
-          ),
-          const Image(
-              image: AssetImage('assets/instructions/InstallAppAndroid.jpg'), fit: BoxFit.cover),
-          const Text(
-            'für Apple (iPhone): Drücken Sie auf den „Teilen“-Button (kleines Viereck mit '
-            'dem Pfeil nach oben) -> Option -> zum Home-Bildschirm',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                backgroundColor: Colors.white,
-                decoration: TextDecoration.none),
-          ),
-          Visibility(
-              visible: false,
-              child: ListTile(
-                leading: const Icon(Icons.token),
-                title: const Text('Installieren'),
-                onTap: () {
-                  PWAInstall().promptInstall_();
-                },
-              )),
-          Visibility(
-              visible: false,
-              child: ElevatedButton(
-                onPressed: () async {
-                  await _showPrompt();
-                },
-                child: const Text('Install'),
-              )),
+          isIosDevice
+              ? const Text(
+                  'Um mit der Studie teilnehmen zu können fügen Sie bitte jetzt diese Seite auf Ihren Homescreen/Startbildschirm hinzu. '
+                  'Im Anschluss können Sie die Seite bzw. das Spiel wie jede gewöhnliche App handhaben. Die Installation funktionier wie folgt (siehe auch Bild):'
+                  'Drücken Sie auf den „Teilen“-Button (kleine Viereck mit dem Pfeil nach '
+                  'oben)   Option  zum Home-Bildschirm',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      backgroundColor: Colors.white,
+                      decoration: TextDecoration.none))
+              : const Text(
+                  'Um mit der Studie teilnehmen zu können fügen Sie bitte jetzt diese Seite auf Ihren Homescreen/Startbildschirm hinzu. Im Anschluss können Sie die Seite bzw. das Spiel wie jede gewöhnliche App handhaben. Die Installation funktionier wie folgt (siehe auch Bild):'
+                  ' Drücken Sie auf die drei kleinen Punkte rechts oben auf dem Bildschirm  App Installieren',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      backgroundColor: Colors.white,
+                      decoration: TextDecoration.none)),
+          isIosDevice
+              ? const Image(
+                  image: AssetImage('assets/instructions/InstallIos.png'), fit: BoxFit.cover)
+              : const Image(
+                  image: AssetImage('assets/instructions/InstallAndroid.png'), fit: BoxFit.cover)
         ]))));
   }
 
