55dba9a95c30d13aaa9c94c9440180f6707401c6
Felpower <felbauer.patrick@gmail.com>
2023-12-10 20:35:12 +0100

    changed icons

--- lib/persistence/dark_patterns_service.dart
+++ lib/persistence/dark_patterns_service.dart
@@ -5,13 +5,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 class DarkPatternsService {
   static const String darkPatterns = 'darkPatterns';
   static int darkPatternsRandomValue = Random().nextInt(2);
-  //ToDo set next int to 7 before release for all dark patterns to be randomly displayed
 
   static Future<int> shouldDarkPatternsBeVisible() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     int? shouldDarkPatternsBeVisible = prefs.getInt(darkPatterns);
     if (shouldDarkPatternsBeVisible == null) {
-      shouldDarkPatternsBeVisible = 1;//ToDo: Change Before Release!!! darkPatternsRandomValue;
+      shouldDarkPatternsBeVisible = darkPatternsRandomValue;
       prefs.setInt(darkPatterns, shouldDarkPatternsBeVisible);
     }
     return shouldDarkPatternsBeVisible;
