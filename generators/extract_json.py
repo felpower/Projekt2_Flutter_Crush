99011c2cbc12c56eb3019f256a4fdfd695291795
Felpower <felbauer.patrick@gmail.com>
2023-12-16 14:30:45 +0100

    continue working on extraction: now every input has it's own row

--- generators/extract_json.py
+++ generators/extract_json.py
@@ -10,12 +10,9 @@ def extract_date_time(timestamp_str):
     try:
         if timestamp_str.startswith("Time: "):
             timestamp_str = timestamp_str.split("Time: ")[-1].replace(" ", "-")
-        # Try to create a datetime object from the ISO formatted string
         timestamp = datetime.fromisoformat(timestamp_str)
-        # Return both the date and time components
         return timestamp.date(), timestamp.time()
     except ValueError:
-        # If there is a ValueError, it means the string was not properly ISO formatted
         return None, None
 
 
@@ -44,53 +41,72 @@ users_data = json_data['users']
 
 for user_id, user_info in users_data.items():
     row = {'userId': user_id,
-           'initAppStartDate': [],
-           'initAppStartTime': [],
-           'appStartDate': [],
-           'appStartTime': [],
-           'level': [],
-           'startOfLevelTime': [],
-           'finishOfLevelTime': [],
-           'levelWon': [],
-           'collectDailyRewardsTime': [],
-           'checkHighscoreTime': [],
-           'pushClick': [],
-           'appCloseTime': [],
-           'appCloseDate': [],
+           'initAppStartDate': "",
+           'initAppStartTime':  "",
+           'appStartDate':  "",
+           'appStartTime':  "",
+           'levelStart':  "",
+           'startOfLevelTime':  "",
+           'levelFinish': "",
+           'finishOfLevelTime':  "",
+           'levelWon':  "",
+           'collectDailyRewardsTime':  "",
+           'checkHighscoreTime':  "",
+           'pushClick':  "",
+           'appCloseTime':  "",
+           'appCloseDate':  "",
            'darkPatterns': int(user_info.get('darkPatterns', 0)) if user_info.get('darkPatterns') is not None else '',
-           'startSurvey': [], }
+           'startSurvey':  "",
+           'endSurvey':  "",
+           }
 
     # Extracting date and time from 'bootAppStartTime' if it exists
     init_start_time = user_info.get('initAppStartTime', None)
     if init_start_time:
         for date_time in init_start_time.values():
             start_date, start_time = extract_date_time(date_time)
-            row['initAppStartDate'].append(str(start_date))
-            row['initAppStartTime'].append(str(start_time))
+            row['initAppStartDate'] = str(start_date)
+            row['initAppStartTime'] = str(start_time)
+            processed_data.append(row.copy())
 
-    # Extracting date and time from 'appStartTime' if it exists
+    row['initAppStartDate'] = ""
+    row['initAppStartTime'] = ""
+
+    # # Extracting date and time from 'appStartTime' if it exists
     app_start_date = user_info.get('appStartDate', None)
     if app_start_date:
         for date_time in app_start_date.values():
             start_date, start_time = extract_date_time(date_time)
-            row['appStartDate'].append(str(start_date))
-            row['appStartTime'].append(str(start_time))
+            row['appStartDate'] = str(start_date)
+            row['appStartTime'] = str(start_time)
+            processed_data.append(row.copy())
+
+    row['appStartDate'] = ""
+    row['appStartTime'] = ""
 
     # Extracting date and time from 'bootAppStartTime' if it exists
     app_close_date = user_info.get('appCloseTime', None)
     if app_close_date:
         for date_time in app_close_date.values():
             start_date, start_time = extract_date_time(date_time)
-            row['appCloseDate'].append(str(start_date))
-            row['appCloseTime'].append(str(start_time))
+            row['appCloseDate'] = str(start_date)
+            row['appCloseTime'] = str(start_time)
+            processed_data.append(row.copy())
+
+    row['appCloseDate'] = ""
+    row['appCloseTime'] = ""
 
     # Processing 'startOfLevel'
     start_of_level = user_info.get('startOfLevel', None)
     if start_of_level:
         for levels in start_of_level.values():
             level, time = levels.split(', ')
-            row['level'].append([int(i) for i in level.split() if i.isdigit()])
-            row['startOfLevelTime'].append(str(extract_date_time(time)[1]))
+            row['levelStart'] = ([int(i) for i in level.split() if i.isdigit()])
+            row['startOfLevelTime'] = (str(extract_date_time(time)[1]))
+            processed_data.append(row.copy())
+
+    row['levelStart'] = ""
+    row['startOfLevelTime'] = ""
 
     # Processing 'finishOfLevel'
     finish_of_level = user_info.get('finishOfLevel', None)
@@ -100,36 +116,52 @@ for user_id, user_info in users_data.items():
                 comma_index = levels.find(' Time:')
                 levels = levels[:comma_index] + "," + levels[comma_index:]
             level, won, time = levels.split(', ')
-            row['levelWon'].append(won.split(': ')[1])
-            row['finishOfLevelTime'].append(str(extract_date_time(time)[1]))
+            row['levelFinish'] = ([int(i) for i in level.split() if i.isdigit()])
+            row['levelWon'] = won.split(': ')[1]
+            row['finishOfLevelTime'] = str(extract_date_time(time)[1])
+            processed_data.append(row.copy())
+    row['levelFinish'] = ""
+    row['levelWon'] = ""
+    row['finishOfLevelTime'] = ""
 
     # Processing 'collectDailyReward'
     daily_rewards = user_info.get('collectDailyRewardsTime', None)
     if daily_rewards:
         for rewards_time in daily_rewards.values():
-            row['collectDailyRewardsTime'].append(str(extract_date_time(rewards_time)[1]))
+            row['collectDailyRewardsTime'] = str(extract_date_time(rewards_time)[1])
+            processed_data.append(row.copy())
+
+    row['collectDailyRewardsTime'] = ""
 
     check_high_score = user_info.get('checkHighScoreTime', None)
     if check_high_score:
         for high_score_time in check_high_score.values():
-            row['checkHighscoreTime'].append(str(extract_date_time(high_score_time)[1]))
+            row['checkHighscoreTime'] = str(extract_date_time(high_score_time)[1])
+            processed_data.append(row.copy())
+
+    row['checkHighscoreTime'] = ""
 
     push_click = user_info.get('pushClick', None)
     if push_click:
         for push_click_time in push_click.values():
-            row['pushClick'].append(str(extract_date_time(push_click_time)[1]))
+            row['pushClick'] = str(extract_date_time(push_click_time)[1])
+            processed_data.append(row.copy())
+
+    row['pushClick'] = ""
 
     start_survey = user_info.get('startSurvey', None)
     if start_survey:
         for start_survey_result in start_survey.values():
-            row['startSurvey'].append(str(start_survey_result.split('[')[1].split(']')[0].split(', ')))
+            row['startSurvey'] = str(start_survey_result.split('[')[1].split(']')[0].split(', '))
+            processed_data.append(row.copy())
 
     end_survey = user_info.get('endSurvey', None)
+
     if end_survey:
         for end_survey_result in end_survey.values():
-            row['endSurvey'].append(str(end_survey_result.split('[')[1].split(']')[0].split(', ')))
-
-    processed_data.append(row)
+            row['endSurvey'] = str(end_survey_result.split('[')[1].split(']')[0].split(', '))
+            processed_data.append(row.copy())
+    processed_data.append({})
 
 # Converting the processed data into a DataFrame
 processed_df = pd.DataFrame(processed_data)
