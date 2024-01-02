a29f8a26d28acea5a3bf057e47eea90bdacacded
Felpower <felbauer.patrick@gmail.com>
2024-01-02 22:16:57 +0100

    updated cron to send to everyone

--- generators/notification_cron.py
+++ generators/notification_cron.py
@@ -8,15 +8,14 @@ firebase_admin.initialize_app(cred)
 def send_notification():
 	# Your message content
 	message = messaging.Message(
-		token="cs2QAWmYyfXc2kI0OJhhg6:APA91bHoDMlytVEyjnqjATnZMc89ElzqGPkJ5PDkbRfPJm3lFp8nsHrru2rH87kOykOAKqy2pl-pTNY2mZjFr-vqfbIIUOv68vYwUB1m_oPAvP8Q1Lsx9Ps8msYCbshHKQ-E6YwJ4qz0",
+		condition="'all' in topics",
 		data={
-			"title": "Test",
-			"body": "Test",
-			"site": "https://flutter-crush-4ece9.web.app/",
+			"title": "JellyFun",
+			"body": "Hallo! Hast du heute schon gespielt?",
+			"site": "https://felpower.github.io/",
 			"click_action": "FLUTTER_NOTIFICATION_CLICK",
 			"id": "1",
 			"status": "done"
-
 		}
 	)
 
