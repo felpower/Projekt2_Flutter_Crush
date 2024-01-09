c419295796353d84ddd7d9922470395bede07119
Felpower <felbauer.patrick@gmail.com>
2024-01-09 17:44:35 +0100

    Changed DarkPatternsValue back

--- lib/persistence/dark_patterns_service.dart
+++ lib/persistence/dark_patterns_service.dart
@@ -10,7 +10,7 @@ class DarkPatternsService {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     int? shouldDarkPatternsBeVisible = prefs.getInt(darkPatterns);
     if (shouldDarkPatternsBeVisible == null) {
-      shouldDarkPatternsBeVisible = 1; // darkPatternsRandomValue;// 1;// ToDo:
+      shouldDarkPatternsBeVisible = darkPatternsRandomValue; // 1;//
       prefs.setInt(darkPatterns, shouldDarkPatternsBeVisible);
     }
     return shouldDarkPatternsBeVisible;
