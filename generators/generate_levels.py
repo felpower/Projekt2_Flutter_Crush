05fdeefba674f3d9e82bbc2ba7bc2b16715e6969
Felpower <felbauer.patrick@gmail.com>
2024-01-10 17:09:54 +0100

    updated levels; Spielzeit nun mit komma; dailyRewards auch ohne XP

--- generators/generate_levels.py
+++ generators/generate_levels.py
@@ -3,18 +3,30 @@ import json
 import random
 
 
-def generate_levels(num_new_levels, max_dim, min_dim, types, colors):
+def generate_dimensions(min_dim, max_dim, max_difference):
+	x_dim = random.randint(min_dim, max_dim)
+	y_dim = random.randint(min_dim, max_dim)
+
+	# Ensure the difference between x_dim and y_dim is no more than max_difference
+	while abs(x_dim - y_dim) > max_difference:
+		y_dim = random.randint(min_dim, max_dim)
+
+	return x_dim, y_dim
+
+
+def generate_levels(num_new_levels, types, colors):
 	new_levels = []
 	for i in range(1, num_new_levels + 1):
 		level_type = types[(i - 1) % len(types)]  # Rotate through level types
 
 		# Calculate dimensions, ensuring they are within the specified range and difference
-		x_dim = min(max(min_dim + ((i - 1) // len(types)), min_dim), max_dim)
-		y_dim = min(max(x_dim - 2, min_dim), max_dim)  # Ensure yDim is within 2 of xDim, but also within bounds
+		x_dim, y_dim = generate_dimensions(6, 10, 2)
 
 		# Choose between 1-4 colors randomly for the obstacleTypes
 		obstacle_types = random.sample(colors, random.randint(1, 4))
-		num_obstacles = random.randint(5, 30)
+		num_obstacles = x_dim * y_dim // 5
+		if level_type == "Colors":
+			num_obstacles = obstacle_types.__len__() * (i // 10 + 1)
 		new_level = {
 			"level": i,
 			"type": level_type,
@@ -39,8 +51,7 @@ max_dimension = 10
 min_dimension = 6
 level_types = ["Timer", "Colors", "Moves", "Obstacle"]
 obstacle_colors = ["Blue", "Purple", "Pink", "Green", "Yellow", "Red"]
-adjusted_levels = generate_levels(num_levels_to_generate, max_dimension, min_dimension, level_types,
-                                  obstacle_colors)
+adjusted_levels = generate_levels(num_levels_to_generate, level_types, obstacle_colors)
 
 # Convert to JSON and save to a file
 adjusted_json_data = json.dumps({"levels": adjusted_levels}, indent=4)
