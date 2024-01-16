fef3443bd0d55dc57089b2bd51596ee0819da562
Felpower <felbauer.patrick@gmail.com>
2024-01-16 11:11:25 +0100

    Update Contact Page

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -10,22 +10,29 @@ class ContactPage extends StatelessWidget {
         child: Scaffold(
             appBar: AppBar(
               automaticallyImplyLeading: true,
-              title: const Text('Kontaktinformationen'),
+              title: const Text('Kontaktinformationen', textAlign: TextAlign.center),
+              centerTitle: true,
             ),
             body: const Center(
                 child: SingleChildScrollView(
               padding: EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  Text('''Arbeiterkammer Niederösterreich''',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text('''E-Mail: jellyfun@aknoe.at'''),
+
                   Text('''Universität Wien''', style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text('''Contact
-Office: Floragasse 7, 5th Floor, 1040 Wien
-Phone: +43 (1) 505 36 88
-Fax: +43(1) 505 88 88
-E-Mail: office@sba-research.org''', textAlign: TextAlign.center),
+                  Text('''
+Floragasse 7, 5. Stock, 1040 Wien
+office@sba-research.org
++43 (1) 505 36 88''', textAlign: TextAlign.center),
+                  SizedBox(height: 20),
+
+                  Text('''Kammer für Arbeiter und Angestellte für Niederösterreich''',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+
+                  Text('''
+AK-Platz 1, 3100 St. Pölten
+jellyfun@aknoe.at
++43 5 7171-0''', textAlign: TextAlign.center),
                 ],
               ),
             ))));
