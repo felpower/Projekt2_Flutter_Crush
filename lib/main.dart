881ca8b7a16be8fc7c5c41e1901a0055990e1c86
Felpower <felbauer.patrick@gmail.com>
2024-02-07 12:16:49 +0100

    added cookie banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:pwa_install/pwa_install.dart';
 import 'package:universal_html/html.dart' as html;
