370223c5224b929b0d650003ef5cf06a8856217d
Felpower <felbauer.patrick@gmail.com>
2024-09-11 11:40:15 +0200

    Check if Add is forced, in order to grant coins

--- lib/controllers/advertisement_video_player.dart
+++ lib/controllers/advertisement_video_player.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:video_player/video_player.dart';
 
-
 class AdvertisementVideoPlayer extends StatefulWidget {
-  const AdvertisementVideoPlayer({Key? key}) : super(key: key);
+  final bool isForcedAd;
+
+  const AdvertisementVideoPlayer({Key? key, required this.isForcedAd}) : super(key: key);
 
   @override
   State<AdvertisementVideoPlayer> createState() => _AdvertisementVideoPlayerState();
 }
-
 class _AdvertisementVideoPlayerState extends State<AdvertisementVideoPlayer> {
   late VideoPlayerController controller;
   bool startedPlaying = false;
@@ -21,6 +22,10 @@ class _AdvertisementVideoPlayerState extends State<AdvertisementVideoPlayer> {
     controller.addListener(() async {
       if (startedPlaying && !controller.value.isPlaying && !popped) {
         popped = true;
+        if (!widget.isForcedAd) {
+          print("Rewarding user with coins");
+          rewardUserWithCoins();
+        }
         Navigator.of(context).pop();
       }
     });
@@ -40,44 +45,62 @@ class _AdvertisementVideoPlayerState extends State<AdvertisementVideoPlayer> {
     return true;
   }
 
+  void rewardUserWithCoins() {
+    // Reward user with coins
+    Fluttertoast.showToast(
+      msg: "You have been rewarded with 10 coins",
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return FutureBuilder<bool>(
       future: started(),
       builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
         if (snapshot.data == true) {
           return Stack(
             children: [
-              VideoPlayer(controller),
+              Positioned.fill(
+                child: FittedBox(
+                  fit: BoxFit.fill,
+                  child: SizedBox(
+                    width: controller.value.size.width,
+                    height: controller.value.size.height,
+                    child: VideoPlayer(controller),
+                  ),
+                ),
+              ),
               GestureDetector(
                 onTapDown: (tapDownEvent) => _onTapDown(tapDownEvent),
               ),
-              Container(
-                  padding: const EdgeInsets.all(5),
-                  width: 35,
-                  height: 35,
-                  margin: const EdgeInsets.only(top: 30),
+                Positioned(
+                  top: 30,
+                  right: 10,
                   child: FutureBuilder(
-                      future: Future.delayed(const Duration(milliseconds: 5000)),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          return TextButton(
-                              child: const Icon(
-                                Icons.close,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              });
-                        } else {
-                          return const Icon(Icons.update, color: Colors.white);
-                        }
-                      }))
+                    future: Future.delayed(const Duration(milliseconds: 5000)),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return TextButton(
+                          child: const Icon(
+                            Icons.close,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        );
+                      } else {
+                        return const Icon(Icons.update, color: Colors.white);
+                      }
+                    },
+                  ),
+                ),
             ],
           );
         } else {
-          return const Text('');
+          return const Center(child: CircularProgressIndicator());
         }
       },
     );
