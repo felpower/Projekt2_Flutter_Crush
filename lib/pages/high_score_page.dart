d7b483cd12f6109ca02778450b6dda53709f4dc2
Felpower <felbauer.patrick@gmail.com>
2024-09-11 12:09:31 +0200

    Dark Patterns Info now displayed when user goes back from HighScore

--- lib/pages/high_score_page.dart
+++ lib/pages/high_score_page.dart
@@ -1,12 +1,10 @@
-// ignore_for_file: avoid_print
+import 'dart:async';
 import 'dart:math';
-
 import 'package:bachelor_flutter_crush/helpers/app_colors.dart';
 import 'package:bachelor_flutter_crush/persistence/high_score_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' as flutter_bloc;
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../bloc/reporting_bloc/reporting_bloc.dart';
 import '../bloc/reporting_bloc/reporting_event.dart';
 import '../model/user.dart';
@@ -30,9 +28,7 @@ class HighScoreState extends State<HighScorePage> {
   @override
   void initState() {
     super.initState();
-
     _loadHighScore();
-    WidgetsBinding.instance.addPostFrameCallback((_) => _showUsernameDialog());
   }
 
   _loadHighScore() async {
@@ -69,95 +65,76 @@ class HighScoreState extends State<HighScorePage> {
     return update;
   }
 
-  void _showUsernameDialog() async {
+  Future<bool> _onWillPop() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? username = prefs.getString('username');
-    var darkPatternsInfoScore = prefs.getBool('darkPatternsInfoScore');
-    if (username == null || username.isEmpty) {
-      final TextEditingController usernameController = TextEditingController();
+    var dpInfoShown = prefs.getBool('darkPatternsInfoScore');
+    if (dpInfoShown == null || dpInfoShown == false) {
+      _showDarkPatternsInfo();
+    } else {
+      Navigator.of(context).pop();
+    }
+    return false;
+  }
+
+  void _showDarkPatternsInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isExpanded = false;
+    var dpInfoShown = prefs.getBool('darkPatternsInfoScore');
+
+    if (dpInfoShown == null || dpInfoShown == false) {
       return showDialog(
         context: context,
         builder: (context) {
-          return AlertDialog(
-            title: const Text('Username eingeben'),
-            content: TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(hintText: "Username"),
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  setState(() {
-                    users[users.indexWhere((element) => element.isUser == true)]
-                        .name = usernameController.text;
-                    prefs.setString('username', usernameController.text);
-                  });
-                  Navigator.of(context).pop();
-                  _showDarkPatternsInfo();
-                },
-              ),
-            ],
+          return StatefulBuilder(
+            builder: (context, setState) {
+              return AlertDialog(
+                title: const Text('Das war gerade ein Dark Pattern!'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                        'Manchmal nutzen Spiele eine gefälschte Highscore-Liste, um Spieler glauben zu lassen, sie treten gegen echte Menschen an.'),
+                    if (isExpanded)
+                      const Text(
+                        'Diese Listen zeigen beeindruckend hohe Punktzahlen, die scheinbar von anderen Spielern erreicht wurden. Doch in Wirklichkeit werden diese Zahlen oft vom Spiel selbst generiert, um dich dazu zu bringen, weiterzuspielen. Der Gedanke, „nur noch ein paar Punkte“ zu machen, um an die Spitze zu kommen, sorgt dafür, dass du immer wieder versuchst, deinen Platz in der Rangliste zu verbessern – obwohl die Konkurrenz gar nicht echt ist.',
+                      ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isExpanded = !isExpanded;
+                        });
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(isExpanded ? "" : 'Mehr erfahren'),
+                          isExpanded
+                              ? const Icon(Icons.expand_less)
+                              : const Icon(Icons.expand_more),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('OK'),
+                    onPressed: () {
+                      prefs.setBool('darkPatternsInfoScore', true);
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
           );
         },
       );
+    } else {
+      Navigator.pop(context);
+      Navigator.pop(context);
     }
-    if (darkPatternsInfoScore == null || darkPatternsInfoScore == false) {
-      _showDarkPatternsInfo();
-    }
-  }
-
-  void _showDarkPatternsInfo() async { //ToDo: Move to back button
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool isExpanded = false;
-    showDialog(
-      context: context,
-      builder: (context) {
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return AlertDialog(
-              title: const Text('Das war gerade ein Dark Pattern!'),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(
-                      'Manchmal nutzen Spiele eine gefälschte Highscore-Liste, um Spieler glauben zu lassen, sie treten gegen echte Menschen an.'),
-                  if (isExpanded)
-                    const Text(
-                      'Diese Listen zeigen beeindruckend hohe Punktzahlen, die scheinbar von anderen Spielern erreicht wurden. Doch in Wirklichkeit werden diese Zahlen oft vom Spiel selbst generiert, um dich dazu zu bringen, weiterzuspielen. Der Gedanke, „nur noch ein paar Punkte“ zu machen, um an die Spitze zu kommen, sorgt dafür, dass du immer wieder versuchst, deinen Platz in der Rangliste zu verbessern – obwohl die Konkurrenz gar nicht echt ist.',
-                    ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isExpanded = !isExpanded;
-                      });
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(isExpanded ? "" : 'Mehr erfahren'),
-                        isExpanded
-                            ? const Icon(Icons.expand_less)
-                            : const Icon(Icons.expand_more),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              actions: <Widget>[
-                TextButton(
-                  child: const Text('OK'),
-                  onPressed: () {
-                    prefs.setBool('darkPatternsInfoScore', true);
-                    Navigator.of(context).pop();
-                    },
-                ),
-              ],
-            );
-          },
-        );
-      },
-    );
   }
 
   void updateUserAndHighScore(SharedPreferences prefs) {
@@ -192,31 +169,43 @@ class HighScoreState extends State<HighScorePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('High Score'),
-              leading: BackButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
+          appBar: AppBar(
+            title: const Text('High Score'),
+            leading: BackButton(
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                var dpInfoShown = prefs.getBool('darkPatternsInfoScore');
+                if (dpInfoShown == null || dpInfoShown == false) {
+                  _showDarkPatternsInfo();
+                } else {
+                  Navigator.of(context).pop();
+                }
+              },
             ),
-            body: Stack(
-              children: <Widget>[
-                Container(
-                    decoration: const BoxDecoration(
-                        image: DecorationImage(
-                  image:
-                      AssetImage('assets/images/background/background_new.png'),
-                  fit: BoxFit.cover,
-                ))),
-                ListView(
-                  children: [_createDataTable()],
-                )
-              ],
-            )));
+          ),
+          body: Stack(
+            children: <Widget>[
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/background/background_new.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              ListView(
+                children: [_createDataTable()],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   DataTable _createDataTable() {
@@ -245,7 +234,7 @@ class HighScoreState extends State<HighScorePage> {
             _isSortAsc = !_isSortAsc;
           });
         },
-      )
+      ),
     ];
   }
 
@@ -253,16 +242,17 @@ class HighScoreState extends State<HighScorePage> {
     List<DataRow> rows = [];
     for (User user in users) {
       rows.add(DataRow(
-          color: user.isUser
-              ? WidgetStateColor.resolveWith(
-                  (states) => AppColors.getColorFromHex("#e52012"))
-              : WidgetStateColor.resolveWith(
-                  (states) => AppColors.getColorFromHex(("#80c9e2"))),
-          cells: [
-            DataCell(Text('#${user.place}')),
-            DataCell(Text(user.name)),
-            DataCell(Text(user.xp.toString()))
-          ]));
+        color: user.isUser
+            ? WidgetStateColor.resolveWith(
+                (states) => AppColors.getColorFromHex("#e52012"))
+            : WidgetStateColor.resolveWith(
+                (states) => AppColors.getColorFromHex(("#80c9e2"))),
+        cells: [
+          DataCell(Text('#${user.place}')),
+          DataCell(Text(user.name)),
+          DataCell(Text(user.xp.toString())),
+        ],
+      ));
     }
     return rows;
   }
